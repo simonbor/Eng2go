ae4c33e18dc17b604dfcaaa61235a38491f66869
Simon <simonbor@gmail.com>
2014-11-02 23:15:17 +0200

    First Ready

--- public/js/yousay.js
+++ public/js/yousay.js
@@ -124,3 +124,14 @@ function addData(index, local, sentence, data, link){
     };
     ys_data.push(item);
 }
+
+// Set/Get cookie are not used temporary
+function setCookie(key, value) {
+    var expires = new Date();
+    expires.setTime(expires.getTime() + (1 * 24 * 60 * 60 * 1000));
+    document.cookie = key + '=' + value + ';expires=' + expires.toUTCString();
+}
+function getCookie(key) {
+    var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
+    return keyValue ? keyValue[2] : null;
+}
